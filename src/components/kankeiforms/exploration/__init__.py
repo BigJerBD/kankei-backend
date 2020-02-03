d96051d49d70d5f7eb5588d81a3456d2290cfee2
Jeremie Bigras-Dunberry <bigjerbd@gmail.com>
2020-02-03 01:04:54 -0500

    feature(kanji_synonyms): add query

--- src/components/kankeiforms/exploration/__init__.py
+++ src/components/kankeiforms/exploration/__init__.py
@@ -3,3 +3,5 @@ from .kanji_linked_to_meaning import KanjiLinkedToMeaning
 from .kanji_linked_to_reading import KanjiLinkedToReading
 from .word_linked_to_meaning import WordLinkedToMeaning
 from .kanji_antonyms import KanjiAntonym
+from .kanji_synonym import KanjiSynonym
+from .word_antonyms import WordAntonym
---
+++ src/components/kankeiforms/exploration/kanji_synonym.py
@@ -0,0 +1,73 @@
+from components.kankeiforms.graph_util import (
+    MEANING_SUBPATHS_PARTIAL,
+    READING_SUB_PATH,
+    get_reading_key,
+)
+from components.kankeiforms.kankeiform import KankeiForm
+from components.kankeiforms.shown_properties import DEFAULT_SHOWN_PROPERTIES
+from components.kankeiforms.transforms import nested_list_transform, two_list_transform
+from tools.queryform.field_types import ChoiceSubField
+from tools.queryform.fields import IntField, StringField
+
+
+class KanjiSynonym(KankeiForm):
+    group = "Exploration"
+    name = "Get synonym (kanji)"
+    tooltip = ""
+    coloring_types = ["Meaning", "Character", "Reading"]
+    fields = [
+        StringField(
+            name="Kanji", template_name="kanji", description="kanji to search around"
+        ),
+        ChoiceSubField(
+            name="English meaning Search",
+            template_name="english_meaning_search",
+            choices={"Yes": [], "No": []},
+            description="Search with the use of english meaning",
+        ),
+        ChoiceSubField(
+            name="Japanese meaning Search",
+            template_name="japanese_meaning_search",
+            choices={"Yes": [], "No": []},
+            description="Search with the use of japanese meaning",
+        ),
+        IntField(
+            name="Synonym limit",
+            template_name="kanji_limit",
+            description="maximum number of path",
+            default=200,
+            validate=lambda x: 0 <= x <= 1000,
+            validate_error_message='"Synonym limit" must be between 0 and 250',
+        )
+    ]
+    transform_output = two_list_transform
+    shown_properties = DEFAULT_SHOWN_PROPERTIES
+
+    @classmethod
+    def get_query(cls, **kwargs):
+        base_query = f"MATCH (k:Character {{writing: '{kwargs['kanji'].value}'}})"
+        to_collect = []
+
+        if kwargs["english_meaning_search"].choice == "Yes":
+            base_query += "OPTIONAL MATCH p0=(k)-[:HasMeaning]-(:English)-[:IsSynonym]-(:Meaning)<-[:HasMeaning]-(:Character) "
+            to_collect.append("p0")
+
+        if kwargs["japanese_meaning_search"].choice == "Yes":
+            base_query += "OPTIONAL MATCH p1=(k)-[:HasMeaning]-(:Japanese)-[:IsSynonym]->(:Kanji) "
+            base_query += "OPTIONAL MATCH p2=(k)-[:IsSynonym]-(:Japanese)<-[:HasMeaning]-(:Kanji) "
+            base_query += "OPTIONAL MATCH p3=(k)-[:HasArchaism]-(:Kanji) "
+            to_collect.append("p1")
+            to_collect.append("p2")
+            to_collect.append("p3")
+
+        to_collect = [f"collect({p})[..{kwargs['kanji_limit'].value}]" for p in to_collect]
+        base_query += f"WITH {' + '.join(to_collect)} AS paths "
+        base_query += (
+            f"UNWIND paths as path "
+            f"WITH collect(nodes(path)) AS nds, collect(relationships(path)) AS lnks "
+            f"WITH apoc.coll.flatten(nds) AS nds, apoc.coll.flatten(lnks) AS lnks "
+            f"UNWIND nds AS nd WITH collect(DISTINCT nd) AS nodes, lnks UNWIND lnks AS lnk "
+            f"RETURN nodes, collect(DISTINCT lnk) AS links;"
+        )
+
+        return (base_query, {})
